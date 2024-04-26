701331408109e347da38ed4f915989cc9ad184b3
Ryan O'Shea <rpo1202@gmail.com>
2024-04-26 17:05:46 -0400

    Added path publishing package and custom planner

---
+++ src/path_pub_package/path_pub_package/path_pub.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Path
+import numpy as np
+
+class MapSolver(Node):
+    def __init__(self):
+        super().__init__('navigation_node')
+
+        self.path_pub = self.create_publisher(Path, 'custom_path', 10)
+
+
+    def pubPath(self):
+        start_x = -2.0
+        end_x = 1.0
+        start_y = -0.5
+        end_y = -0.5
+
+        int_x = np.linspace(start_x, end_x, 25)
+        int_y = np.linspace(start_y, end_y, 25)
+
+        # Create blank path message
+        path_msg = Path()
+        path_msg.header.stamp = self.get_clock().now().to_msg()
+        path_msg.header.frame_id = 'map'
+
+        waypoints = []
+
+        for (x, y) in zip(int_x, int_y):
+            pose_stamped = PoseStamped()
+            # pose_stamped.header.stamp = self.get_clock().now().to_msg()
+            # pose_stamped.header.frame_id = 'map'  # Set the frame ID
+            pose_stamped.pose.position.x = x  # Set x-coordinate
+            pose_stamped.pose.position.y = y  # Set y-coordinate
+            pose_stamped.pose.position.z = 0.0   # Set z-coordinate
+            waypoints.append(pose_stamped)
+        # Populate the Path message with the waypoints
+        path_msg.poses = waypoints
+
+        print("Publishing")
+        # print(path_msg)
+
+        # Publish the Path message
+        self.path_pub.publish(path_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MapSolver()
+    try:
+        node.pubPath()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()    
+
+if __name__ == "__main__":
+    main()
---
+++ src/path_pub_package/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'path_pub_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ryan',
+    maintainer_email='rpo1202@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'path_pub = path_pub_package.path_pub:main'
+        ],
+    },
+)
